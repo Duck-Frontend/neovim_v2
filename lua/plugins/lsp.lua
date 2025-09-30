079a8cc57d8e5aa6c3136dd1d9fc8f2f40aaa105
Anton Stadolnik <stadonikanton1@gmail.com>
2025-09-30 20:41:53 +0300

    Изменение lsp для добавление работы frontend

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,21 +5,30 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Установка LSP серверов через Mason
 mason_lspconfig.setup({
   ensure_installed = {
+    -- Бэкенд
     "pylsp",        -- Python (альтернатива: "pyright")
-    "clangd",       -- C/C++  
+    "clangd",       -- C/C++
     "pylint",
-    "pyflakes"
+    "pyflakes",
+    
+    -- Фронтенд
+    "tsserver",           -- TypeScript/JavaScript
+    "html",               -- HTML
+    "cssls",              -- CSS
+    "emmet_ls",           -- Emmet
+    "jsonls",             -- JSON
+    "eslint",             -- ESLint
+    "vue_language_server", -- Vue.js
+    "tailwindcss"         -- Tailwind CSS (опционально)
+  }
 })
 
 -- Функция для автоматического определения venv
 local function get_python_path()
-  -- Проверяем наличие venv в корне проекта
   local venv_path = vim.fn.getcwd() .. '/.venv'
   if vim.fn.isdirectory(venv_path) == 1 then
-    return venv_path .. '/bin/python'  -- Linux/MacOS
-    -- Для Windows: venv_path .. '\\Scripts\\python.exe'
+    return venv_path .. '/bin/python'
   end
-  -- Если venv нет, используем глобальный Python
   return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
 end
 
@@ -29,23 +38,18 @@ lspconfig.pylsp.setup({
   settings = {
     pylsp = {
       plugins = {
-        pycodestyle = { enabled = false },  -- Отключаем pycodestyle (если мешает)
-        pylint = { enabled = true },        -- Включаем pylint
-        pyflakes = { enabled = true },      -- Включаем pyflakes
+        pycodestyle = { enabled = false },
+        pylint = { enabled = true },
+        pyflakes = { enabled = true },
         jedi_completion = { enabled = true },
         rope_autoimport = { enabled = true },
         jedi = {
-          environment = get_python_path(),  -- Указываем путь к Python из venv
-          extra_paths = { "./src" },       -- Доп. пути для импортов
+          environment = get_python_path(),
+          extra_paths = { "./src" },
         },
       },
     },
   },
-  on_attach = function(client, bufnr)
-    -- Ключевые маппинги для LSP
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true })
-  end,
 })
 
 -- Настройка clangd (C/C++)
@@ -59,11 +63,76 @@ lspconfig.clangd.setup({
     "--completion-style=detailed",
     "--function-arg-placeholders",
     "--fallback-style=llvm"
-  },
+  }
+})
+
+-- Фронтенд LSP серверы
+
+-- TypeScript/JavaScript
+lspconfig.tsserver.setup({
+  capabilities = capabilities,
   on_attach = function(client, bufnr)
-    -- Специфичные для C++ keymaps
-    vim.keymap.set('n', '<leader>lh', '<cmd>ClangdSwitchSourceHeader<cr>', { buffer = bufnr })
+    -- Отключаем tsserver форматирование, используем Prettier
+    client.server_capabilities.documentFormattingProvider = false
   end
+})
+
+-- HTML
+lspconfig.html.setup({
+  capabilities = capabilities,
+  filetypes = { 'html', 'erb', 'ejs' }
+})
+
+-- CSS
+lspconfig.cssls.setup({
+  capabilities = capabilities
+})
+
+-- Emmet LSP
+lspconfig.emmet_ls.setup({
+  capabilities = capabilities,
+  filetypes = { 
+    'html', 
+    'typescriptreact', 
+    'javascriptreact', 
+    'css', 
+    'sass', 
+    'scss', 
+    'less', 
+    'vue' 
+  }
+})
+
+-- JSON
+lspconfig.jsonls.setup({
+  capabilities = capabilities,
+  settings = {
+    json = {
+      schemas = require('schemastore').json.schemas(),
+      validate = { enable = true }
+    }
+  }
+})
+
+-- ESLint
+lspconfig.eslint.setup({
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
+
+-- Vue.js (если используете)
+lspconfig.vue_language_server.setup({
+  capabilities = capabilities
+})
+
+-- Tailwind CSS (опционально)
+lspconfig.tailwindcss.setup({
+  capabilities = capabilities
 })
 
 -- Общий обработчик для LSP
@@ -78,8 +147,29 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
 
-    -- Динамическое обновление конфигурации (особенно для Python)
+    -- Форматирование (кроме tsserver)
+    if client.name ~= 'tsserver' then
+      vim.keymap.set('n', '<leader>f', function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
+    else
+      -- Для TypeScript используем отдельное форматирование
+      vim.keymap.set('n', '<leader>f', function()
+        vim.cmd('Prettier')
+      end, opts)
+    end
+
+    -- Специфичные keymaps для C++
+    if client.name == 'clangd' then
+      vim.keymap.set('n', '<leader>lh', '<cmd>ClangdSwitchSourceHeader<cr>', opts)
+    end
+
+    -- Динамическое обновление конфигурации для Python
     if client.name == 'pylsp' then
       client.config.settings = {
         pylsp = {
@@ -93,3 +183,38 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end
 })
+
+-- Дополнительная настройка форматирования
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    -- Форматирование
+    null_ls.builtins.formatting.prettier.with({
+      filetypes = { 
+        "javascript", 
+        "javascriptreact", 
+        "typescript", 
+        "typescriptreact", 
+        "vue", 
+        "css", 
+        "scss", 
+        "less", 
+        "html", 
+        "json", 
+        "yaml", 
+        "markdown" 
+      },
+    }),
+    
+    -- Линтинг
+    null_ls.builtins.diagnostics.eslint_d,
+    
+    -- Code actions
+    null_ls.builtins.code_actions.eslint_d,
+  },
+})
+
+-- Глобальное форматирование (для файлов без LSP)
+vim.keymap.set('n', '<leader>F', function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = 'Format file' })
