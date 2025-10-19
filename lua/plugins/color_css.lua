dd77d4bc458e780eca008478a8c28ae63e21d594
Anton Stadolnik <stadonikanton1@gmail.com>
2025-10-19 17:39:38 +0300

    Добавление файлов css цветов

---
+++ lua/plugins/color_css.lua
@@ -0,0 +1,12 @@
+require('nvim-highlight-colors').setup()
+
+-- Или для colorizer.lua
+require('colorizer').setup({
+  'css',
+  'scss',
+  'sass',
+  'less',
+  'html',
+  'javascript',
+  'javascriptreact'
+})
